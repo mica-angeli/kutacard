dd6091677473537512c3b8511d04ad3a52f29fec
Ricardo Angeli <rangeli93@gmail.com>
2020-05-11 05:32:16 -0400

    Changed shift-jis to ascii conversion to generate and use a table

--- include/shiftjis.h
+++ include/shiftjis.h
@@ -3,30 +3,41 @@
 
 #include <sstream>
 #include <string>
+#include <unordered_map>
 
 namespace shiftjis
 {
+  std::unordered_map<uint16_t, char> generateConversionTable()
+  {
+    std::unordered_map<uint16_t, char> map;
+
+    // Numbers
+    for (uint16_t c = 0x824f; c <= 0x8258; c++)
+      map[c] = char(c - 0x824f + 0x30);
+
+    // Capital letters
+    for (uint16_t c = 0x8260; c <= 0x8279; c++)
+      map[c] = char(c - 0x8260 + 0x41);
+
+    // Lowercase letters
+    for (uint16_t c = 0x8281; c <= 0x829a; c++)
+      map[c] = char(c - 0x8281 + 0x61);
+
+    return map;
+  }
 
   std::string toAscii(const std::string& input)
   {
+    static const auto conversionTable = generateConversionTable();
+
     std::ostringstream oss;
     for(auto it = input.begin(); it != input.end() and std::next(it) != input.end(); it += 2)
     {
       const uint16_t c = (static_cast<uint8_t>(*it) << 8) + static_cast<uint8_t>(*std::next(it));
-      if(c >= 0x824f and c <= 0x8258)
+      
+      if(conversionTable.find(c) != conversionTable.end())
       {
-        // Number
-        oss << char(c - 0x824f + 0x30);
-      }
-      else if(c >= 0x8260 and c <= 0x8279)
-      {
-        // Capital letter
-        oss << char(c - 0x8260 + 0x41);
-      }
-      else if(c >= 0x8281 and c <= 0x829a)
-      {
-        // Lowercase letter
-        oss << char(c - 0x8281 + 0x61);
+        oss << conversionTable.at(c);
       }
       else
       {
